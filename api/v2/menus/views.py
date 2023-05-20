8e417e85c2841ec63860eb3645d6b4f127686dc9
kameleon-ad <alvindelos439@outlook.com>
2023-05-20 05:32:06 -0500

    Created `/api/v2/menus/today` api to get today's menu

--- api/v2/menus/views.py
+++ api/v2/menus/views.py
@@ -2,7 +2,9 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.request import Request
 from rest_framework.response import Response
+from rest_framework.decorators import action
 from rest_framework import status
+from datetime import date
 from .models import Menus
 from .serializers import MenusSerializer
 
@@ -15,10 +17,10 @@ class MenusViewSet(ModelViewSet):
 
     def create(self, request: Request, *args, **kwargs):
         restaurant_id = request.data.get('restaurant')
-        date = request.data.get('date')
-        menu_exists = Menus.objects.filter(restaurant=restaurant_id, date=date).exists()
+        req_date = request.data.get('date')
+        menu_exists = Menus.objects.filter(restaurant=restaurant_id, date=req_date).exists()
         if menu_exists:
-            menu = Menus.objects.get(restaurant=restaurant_id, date=date)
+            menu = Menus.objects.get(restaurant=restaurant_id, date=req_date)
             serializer = self.get_serializer(menu, data=request.data)
         else:
             serializer = self.get_serializer(data=request.data)
@@ -28,3 +30,9 @@ class MenusViewSet(ModelViewSet):
         headers = self.get_success_headers(serializer.data)
 
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    @action(detail=False, methods=['get'])
+    def today(self, request: Request):
+        menus_today = Menus.objects.filter(date=date.today())
+        serializer = MenusSerializer(menus_today, many=True)
+        return Response(serializer.data)
