e7baacc07e8b4e840225eb9b5b3b4ee08d4c5af4
kameleon-ad <alvindelos439@outlook.com>
2023-05-20 22:54:38 -0500

    Completed version control for api: redirect by version

--- api/middleware.py
+++ api/middleware.py
@@ -1,13 +1,18 @@
-from django.shortcuts import redirect
+from django.http import HttpRequest
 
 
 class VersionControlRedirectMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
-    def __call__(self, request):
-        if request.path_info.startswith('/api/def'):
-            request.path_info = '/api/v2' + request.path_info[len('/api/def'):]
+    def __call__(self, request: HttpRequest):
 
+        if not request.path_info.startswith('/api/def'):
+            return self.get_response(request)
+
+        version = request.headers.get('X-API-Version')
+        if version is None:
+            version = 2
+        request.path_info = f'/api/v{ version }' + request.path_info[len('/api/def'):]
         response = self.get_response(request)
         return response
